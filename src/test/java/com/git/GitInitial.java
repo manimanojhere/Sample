caf777cce429e89be181194cb24236b4ffbce0af
manimanojhere <itsmemanimanoj@gmail.com>
2022-12-09 14:45:45 +0530

    git initiated

---
+++ src/test/java/com/git/GitInitial.java
@@ -0,0 +1,16 @@
+package com.git;
+
+public class GitInitial {
+
+	public void git1() {
+		System.out.println("git initialised");
+    }
+	public void git2() {
+		System.out.println("git");
+    }
+	public void git3() {
+		System.out.println("git pushed ");
+    }
+	
+	
+}
